c5f76d48770e00bd8e92da8e0458447dfe03e7d2
msundr <meera.sundar@gmail.com>
2013-11-18 07:05:55 +0530

    Initial Commit

---
+++ SpriteKitSimpleGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitSimpleGame
+//
+//  Created by Meera Sundar on 5/1/13.
+//  Copyright (c) 2013 com.behagdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitSimpleGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpriteKitSimpleGame
+//
+
+//  Copyright (c) 2013 com.behagdev. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
